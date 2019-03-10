d8f2bf176bc487d3c7373a0cd6bc313895493472
diecaptain <kranthi.nitr@gmail.com>
2019-03-09 20:23:06 -0500

    Dual Port 2 Deep RAM

--- FIFO/k_dp_2deep_ram_t1.v
+++ FIFO/k_dp_2deep_ram_t1.v
@@ -7,6 +7,7 @@ module k_dp_2deep_ram_t1 (q, d, wen, waddr, raddr, clk);
     input [data_size-1:0] d;
     input wen, waddr, raddr, clk;
 
+    reg [data_size-1:0] q;
     reg [data_size-1:0] mem [0:addr_size-1];
 
     always @ (posedge clk) begin
