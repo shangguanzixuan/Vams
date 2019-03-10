3b8af4ee35afa5a7fc7582d908d06ae9e362b8b9
diecaptain <kranthi.nitr@gmail.com>
2019-03-10 19:03:07 -0400

    2 FF Synchronizer

--- FIFO/k_Sync2_t1.v
+++ FIFO/k_Sync2_t1.v
@@ -7,11 +7,12 @@ module k_Sync2_t1 (q, d, clk, rst_n);
     reg q1;
 
     always @ (posedge clk or negedge rst_n) begin
-        if (!rst_n)
+        if (!rst_n) begin
             q1 <= 1'b0;
             q <= 1'b0;
-        else
+        end else begin
             q1 <= d;
             q <= q1;
+        end
     end
 endmodule
