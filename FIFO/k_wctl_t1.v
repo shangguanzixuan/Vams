1b6c7f58d3a82c07051099c9ee50d9365819c0f6
diecaptain <kranthi.nitr@gmail.com>
2019-03-10 18:27:05 -0400

    FIFO Synchronizer Write Control

--- FIFO/k_wctl_t1.v
+++ FIFO/k_wctl_t1.v
@@ -1,11 +1,10 @@
 `timescale 1ns/1ps
 
 module k_wctl_t1(
-  output wrdy, wptr, wen,
-  input wput, wq2_rptr,
-  input wclk, wrst_n);
-
-  reg wptr;
+  output wire wrdy, wen,
+  output reg wptr,
+  input wire wput, wq2_rptr,
+  input wire wclk, wrst_n);
 
   always @ (posedge wclk or negedge wrst_n) begin
       if (!wrst_n)
